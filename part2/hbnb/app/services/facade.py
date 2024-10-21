51f185fcc9360e97ce1bd197a4155d9a2743d434
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-10-21 17:22:40 -0300

    task 2 progress

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,4 +1,5 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
 
 class HBnBFacade:
     def __init__(self):
@@ -9,10 +10,20 @@ class HBnBFacade:
 
     # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        new_user = User(
+            first_name=user_data['first_name'],
+            last_name=user_data['last_name'],
+            email=user_data['email'])
+        self.user_repo.add(new_user)
+        return new_user
+
+    def get_user(self, id):
+        return self.user_repo.get(id)
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
